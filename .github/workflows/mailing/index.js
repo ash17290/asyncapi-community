e7366158e2a2842ef9c00c1e0ff46b347cd81718
ash17290 <136432476+ash17290@users.noreply.github.com>
2025-02-12 15:26:40 +0530

    temp
    Signed-off-by: Ashish Padhy <ashishpadhy1729@gmail.com>

---
+++ .github/workflows/mailing/index.js
@@ -0,0 +1,34 @@
+const mailer = require('@sendgrid/mail');
+const { htmlMailContent, htmlToText } = require('./content');
+
+mailer.setApiKey(process.env.SENDGRID_API_KEY);
+const from = 'ashishpadhy1729@gmail.com';
+
+const sendMail = async (to, type, name, links, title, custom = {}) => {
+  const { subject, html } = htmlMailContent(type, name, links, title, custom);
+  const text = htmlToText(html);
+
+  const msg = {
+    to,
+    from,
+    subject,
+    text,
+    html,
+  };
+
+  try {
+    const response = await mailer.send(msg);
+    console.debug(response);
+
+    return {
+      success: response[0].statusCode === 202,
+      message: response[0].body,
+    };
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = {
+  sendMail,
+};
